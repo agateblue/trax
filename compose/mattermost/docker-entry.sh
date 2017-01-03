c1ef86976fe5a8333945258c722e73f40d661e7b
Eliot Berriot <contact@eliotberriot.com>
2017-01-03 18:06:44 +0100

    Now include a mattermost server via docker for easier dev / test

---
+++ compose/mattermost/docker-entry.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
+# See License.txt for license information.
+
+echo "Starting MySQL"
+/entrypoint.sh mysqld &
+
+until mysqladmin -hlocalhost -P3306 -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" processlist &> /dev/null; do
+	echo "MySQL still not ready, sleeping"
+	sleep 5
+done
+
+echo "Starting platform"
+cd mattermost
+exec ./bin/platform -config=config/config_docker.json
